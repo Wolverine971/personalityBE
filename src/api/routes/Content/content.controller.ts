a6de4f84b95698f072f5b3c94d75fd824cfd5611
David Wayne <djwayne35@gmail.com>
2021-01-16 23:47:16 -0500

    create content bug

--- src/api/routes/Content/content.controller.ts
+++ src/api/routes/Content/content.controller.ts
@@ -63,7 +63,7 @@ export async function addContent(req: Request, res: Response) {
             likes
             dateCreated
             comments{
-              comment{
+              comments{
                 id
                 comment
                 likes
